4d4a54ce64b94e906e9e39884d4d190f930b1339
Dāvis Krēgers <daviskregers@protonmail.com>
2022-05-11 19:30:39 +0300

    mumbling

---
+++ 7kyu-mumbling/main.rs
@@ -0,0 +1,37 @@
+fn accum(s:&str)->String {
+    let mut result : Vec<String> = vec![];
+    s.chars().enumerate().for_each(|(i, x)| {
+        let first : String = x.to_uppercase().to_string();
+        let second : String = (0..i)
+            .map(|_| x.to_lowercase().to_string())
+            .collect();
+        result.push(first + &second)
+    });
+
+    result.join("-")
+}
+
+fn accum2(s:&str) -> String {
+    s.chars().enumerate()
+        .map(|(i,c)| c.to_string().to_uppercase() + c.to_string().to_lowercase().repeat(i).as_str())
+        .collect::<Vec<String>>()
+        .join("-")
+}
+
+#[test]
+fn basic_tests() {
+  assert_eq!(accum("ZpglnRxqenU"), "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu");
+  assert_eq!(accum("NyffsGeyylB"), "N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb");
+  assert_eq!(accum("MjtkuBovqrU"), "M-Jj-Ttt-Kkkk-Uuuuu-Bbbbbb-Ooooooo-Vvvvvvvv-Qqqqqqqqq-Rrrrrrrrrr-Uuuuuuuuuuu");
+  assert_eq!(accum("EvidjUnokmM"), "E-Vv-Iii-Dddd-Jjjjj-Uuuuuu-Nnnnnnn-Oooooooo-Kkkkkkkkk-Mmmmmmmmmm-Mmmmmmmmmmm");
+  assert_eq!(accum("HbideVbxncC"), "H-Bb-Iii-Dddd-Eeeee-Vvvvvv-Bbbbbbb-Xxxxxxxx-Nnnnnnnnn-Cccccccccc-Ccccccccccc");
+}
+
+#[test]
+fn basic_tests2() {
+  assert_eq!(accum2("ZpglnRxqenU"), "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu");
+  assert_eq!(accum2("NyffsGeyylB"), "N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb");
+  assert_eq!(accum2("MjtkuBovqrU"), "M-Jj-Ttt-Kkkk-Uuuuu-Bbbbbb-Ooooooo-Vvvvvvvv-Qqqqqqqqq-Rrrrrrrrrr-Uuuuuuuuuuu");
+  assert_eq!(accum2("EvidjUnokmM"), "E-Vv-Iii-Dddd-Jjjjj-Uuuuuu-Nnnnnnn-Oooooooo-Kkkkkkkkk-Mmmmmmmmmm-Mmmmmmmmmmm");
+  assert_eq!(accum2("HbideVbxncC"), "H-Bb-Iii-Dddd-Eeeee-Vvvvvv-Bbbbbbb-Xxxxxxxx-Nnnnnnnnn-Cccccccccc-Ccccccccccc");
+}
